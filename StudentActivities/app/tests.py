0306b93426b00d78c900866d9e08620109bd1f14
carlos <chaiyukuan@163.com>
2019-03-27 18:58:53 +0800

    sql插入

--- StudentActivities/app/tests.py
+++ StudentActivities/app/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from . import models
 
 # Create your tests here.
+
+# 团长
+def insertLeader(phone, child_name, name, age, event_id):
+    leader = models.Student(phone=phone, child_name=child_name, name=name, age=age, event_id=event_id)
+    leader.save()
+    leader.gid = leader.id
+    leader.save()
+
+# 团员
+def insertMember(phone, child_name, name, age, event_id, gid):
+    member = models.Student(gid=gid, phone=phone, child_name=child_name, name=name, age=age, event_id=event_id)
+    member.save()
+
+def queryLeaders():
+    models.User.objects.filter(gid=request.POST['user']).values()
--- StudentActivities/app/views.py
+++ StudentActivities/app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from . import models
+from . import tests
 
 # Create your views here.
 
@@ -8,8 +9,17 @@ def login(request):
 
 def login2(request):
     if request.method == 'POST':
-        vals = models.User.objects.filter(request.POST['user'])
-        print ("query " + vals)
+        #print (str(models.User.objects.filter(user="chai").query))
+        values = models.User.objects.filter(user=request.POST['user']).values()
+        passwd = request.POST['passwd']
+
+        #print ("query:{0} ,post:{1}".format(values[0]['passwd'], request.POST['passwd']))
+        if (values[0]['passwd'] == request.POST['passwd']):
+            pass
+        else:
+            print ("passwd error")
+        tests.insertLeader("15971111111", "child", "pid", 25, 10)
+        tests.insertMember("15971111111", "child", "pid", 25, 10, 1000)
         return render(request, 'index.html')
 
 def index(request):
